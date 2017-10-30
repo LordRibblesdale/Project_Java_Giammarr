2e032bea4a83c978a1103221968d897ce96c3856
Domenico Verde <domenico.verde@aol.com>
2017-10-30 22:21:21 +0100

    Ese09_1 change
    + Add lines Ese09_1

---
+++ Ese09/Ese09_1.java
@@ -0,0 +1,14 @@
+//Domenico Verde
+/*
+ Esercizio 1 Scrivere un programma che chiede all'utente di inserire un numero x tra 9 e 99
+  (mettere un controllo sull'input), poi genera in modo random 100 numeri tra 9 e 99 e dice all'utente quante volte è stato generato il numero x da lui scelto.
+  */
+
+  import java.util.Random;
+  import java.util.Scanner;
+
+  public class Ese09_1 {
+    public static void main(String[] args) {
+      int x//TODO
+    }
+  }
