d1026643ca2a7bf6736849ae57f955d22de5cc4f
Domenico Verde <domenico.verde@aol.com>
2017-10-31 19:01:25 +0100

    Ese09 changes
    + Add lines Ese09_1

--- Ese09/Ese09_1.java
+++ Ese09/Ese09_1.java
@@ -9,6 +9,32 @@
 
   public class Ese09_1 {
     public static void main(String[] args) {
-      int x//TODO
+      int x, rnd, count = 0;
+      String str = "Il numero e' stato generato ", str2 = " volte";
+
+      Scanner keyboard = new Scanner(System.in);
+      Random random = new Random();
+
+      do {
+        System.out.print("Inserisci un numero (tra 9 e 99): ");
+        x = keyboard.nextInt();
+      }
+      while (x < 9 || x > 99);
+
+      for (int count2 = 1; count2 <= 100; count2++) {
+        rnd = random.nextInt(91) + 9;
+        //System.out.println(rnd);
+        if (x == rnd) {
+          count++;
+        }
+      }
+
+      if (count == 0) {
+        System.out.println("Il numero non e' mai stato generato.");
+      }
+      else if (count == 1) {
+        System.out.println(str + count + str2.substring(0, 5) + "a");
+      }
+      else System.out.println(str + count + str2.substring(0, 5) + "e");
     }
   }
