c386fd2fedd6a990ab514fe4a50e7b5a88c5fd81
Domenico Verde <domenico.verde@aol.com>
2017-11-02 16:27:22 +0100

    Add Tut01.java

---
+++ Tut1/Tut01.java
@@ -0,0 +1,20 @@
+//Domenico Verde
+
+public class Tut01 {
+	public static void main(String[] args) {
+
+		for (int primeNum = 2;;primeNum++) {
+			for (int num = 10000000; num < 100000000; num++) {
+				for (int primeDiv = primeNum/2; primeDiv > 1; primeDiv--) {
+					if (primeNum % primeDiv != 0) {
+						
+						
+					}
+
+				}
+
+			} 
+		}
+		
+	}
+}
