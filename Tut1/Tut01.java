289cfc415db477ef25a5d64e88e0fd6277946a5a
Domenico Verde <domenico.verde@aol.com>
2017-11-05 19:01:38 +0100

    + Add Tut01.java

---
+++ Tut1/Tut01.java
@@ -0,0 +1,47 @@
+//Domenico Verde
+public class Tut01 {
+  public static void main(String[] args) {
+    String sameNum = "";
+    boolean isFoundNum = false, isPrimeNum = true;
+    int primeNum, num, primeDiv = 2;
+
+    for (num = 10000000; num < 100000000; num++) {
+      primeNum = 3;
+
+      sameNum = sameNum.valueOf(num*primeNum);
+
+      if (sameNum.equals("111111111")) {
+        isFoundNum = true;
+        System.out.println(num + "*" + primeNum + " = " + sameNum);
+        System.exit(0);
+      }
+    }
+
+    while (!isFoundNum) {
+        for (num = 10000000; num < 100000000; num++) {
+          for (primeNum = 5; primeDiv < primeNum/2 && isPrimeNum; primeDiv++) {
+            if (primeNum % primeDiv != 0) {
+              isPrimeNum = true;
+            }
+            else {
+              isPrimeNum = false;
+            }
+            if (isPrimeNum) {
+              sameNum = sameNum.valueOf(num*primeNum);
+
+              if (sameNum.equals("111111111")) {
+                isFoundNum = true;
+           }
+          }
+          if (!isFoundNum) {
+            primeNum++;
+          }
+          else {
+            System.out.println(num + "*" + primeNum + " = " + sameNum);
+            System.exit(0);
+          }
+        }
+      }
+    }
+  }
+}
