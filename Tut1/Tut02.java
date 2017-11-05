f5ebdb9be982a99161ecc7fb3a2473d4f4d04d76
Domenico Verde <domenico.verde@aol.com>
2017-11-05 21:27:57 +0100

    + Add Tut02.java

---
+++ Tut1/Tut02.java
@@ -0,0 +1,13 @@
+// Domenico Verde
+
+import java.util.Random;
+
+public class Tut02 {
+  public static void main(String[] args) {
+    public static final double
+
+    Random randomNum = new Random();
+
+
+  }
+}
