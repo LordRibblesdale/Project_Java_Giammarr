ea6ff7a2d3535b6e49531c046b6ebf0515da1c3c
Domenico Verde <domenico.verde@aol.com>
2017-11-09 16:00:28 +0100

    + Fix workflow Ese10_1

--- Ese10/Ese10_1.java
+++ Ese10/Ese10_1.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class Ese10_1 {
   public static boolean isFound = false;
+  public static int res;
 
   public static int bruteMCD(int x, int y) {
     int mcd = 0;
@@ -24,15 +25,16 @@ public class Ese10_1 {
   public static int zeroTest(int x, int y) {
     if (x == 0) {
       isFound = true;
-      return y;
-    }   
+      res = y;
+    }
     else if (y == 0) {
       isFound = true;
-      return x;
+      res = x;
     }
     else {
       isFound = false;
     }
+    return res;
   }
 
   public static int euclidean(int x, int y) {
@@ -42,46 +44,49 @@ public class Ese10_1 {
         int x1 = y;
         int y1 = x%y;
         if (x1 == 0) {
-    	  return y1;
-        }
- 	else if (y1 == 0) {
-	  return x1;        
-        }
-	else {
-	  euclidean(x1, y1);
-	}
-      }
-      else {
- 	int y1 = x;
-	int x1 = x%y;
-        if (x1 == 0) {
-          return y1;
+          res = y1;
         }
         else if (y1 == 0) {
-          return x1;
+          res = x1;
+        }
+        else {
+          euclidean(x1, y1);
+        }
+      }
+      else {
+        int y1 = x;
+        int x1 = x%y;
+        if (x1 == 0) {
+          res = y1;
+        }
+        else if (y1 == 0) {
+          res = x1;
         }
         else {
           euclidean(x1, y1);
         }
       }
     }
+    return res;
   }
-
   public static void main(String[] args) {
+    int resFinal;
     boolean isFound = false;
 
     Scanner keyboard = new Scanner(System.in);
 
+    System.out.println("Scrivi due valori per trovarne il MCD.");
+    System.out.print("\nInserisci il primo: ");
     int input = keyboard.nextInt();
+    System.out.print("\nInserisci il secondo: ");
     int input2 = keyboard.nextInt();
 
-/*
-    int res = bruteMCD(input, input2);
-    System.out.println(res);
-*/
+    System.out.println("Quale tipo di MCD vuoi eseguire?"); //TODO finire switch e controllo input
+    resFinal = bruteMCD(input, input2);
+    System.out.println(resFinal);
 
-    int res = euclidean(input, input2);
-    System.out.println(res);
+    resFinal = euclidean(input, input2);
+    System.out.println(resFinal);
 
   }
 }
