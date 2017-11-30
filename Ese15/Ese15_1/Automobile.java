24e73718bb6baaa466ad4b8ccc6c6be4079cf79e
Domenico Verde <domenico.verde@aol.com>
2017-11-30 10:21:04 +0100

    + Add Ese14 & 15

---
+++ Ese15/Ese15_1/Automobile.java
@@ -0,0 +1,26 @@
+//
+import java.util.Scanner;
+public class Automobile{
+
+private double serbatoio;
+private int marcia;
+private double kmTot=0;
+
+// Scrivere i metodi setSerbatoio,setMarcia,getSerbatoio,getMarcia
+// NON scriviamo metodi set e get per kmTot perchè il contachilometri non va modificato con assegnazioni
+
+
+// Scrivere i metodi
+
+public boolean consumo(double km){
+	//Questo metodo riduce il livello di carburante in base alla marcia inserita e ai KM da percorrere,
+	// Se non c'e' abbastanza carburante per percorrere i km richiesti, non esegue e restituisce false
+
+	}
+
+
+	public void scriviOutput(){
+		//Questo metodo scrive sullo schermo il valore del contachilometri e la quantità di carburante nel serbatoio
+
+	}
+}
