fe454c02dec408eafc6210285cfecd11df1b0793
Domenico Verde <domenico.verde@aol.com>
2017-12-09 16:16:37 +0100

    move method
    + Add moving methods

--- VerdeTest/VerdeTest_Scacchiera.java
+++ VerdeTest/VerdeTest_Scacchiera.java
@@ -27,12 +27,13 @@ public class VerdeTest_Scacchiera {
 
       switch (b1) {
         case 0: {
-          indexB1 = {b, d};
-
+          indexB1[0] = b;
+          indexB1[1] = d;
           break;
         }
         case 1: {
-          indexB2 = {b, d};
+          indexB2[0] = b;
+          indexB2[1] = d;
           break;
         }
       }
@@ -59,11 +60,13 @@ public class VerdeTest_Scacchiera {
 
         switch (count) {
           case 0: {
-            indexN1 = {b, d};
+            indexN1[0] = b;
+            indexN1[1] = d;
             break;
           }
           case 1: {
-            indexN2 = {b, d};
+            indexN2[0] = b;
+            indexN2[1] = d;
             break;
           }
         }
@@ -79,42 +82,129 @@ public class VerdeTest_Scacchiera {
       for (int j = 0; j < 8; j++) {
         System.out.print("  " + a[i][j] + "  ");
       }
-      System.out.println();
+      System.out.println("");
     }
+    System.out.println("\n");
   }
 
-  public static void move(char[][] a, char c) {
-    int[] numsN = new int[2], numsB = new int[2];
-    int count1 = 0, count2 = 0;
+  public static boolean move(char[][] a, int[] index, char c) {
+    boolean move = false;
+
+    moving:
     switch (c) {
       case 'B': {
-        for (int i = 0; i < 8; i++) {
-          count1 = 0;
-          count2 = 0;
-            for (int j = 0; j < 8; j++) {
-              if (a[i][j] == 'N') {
-                numsN[count1] = j;
-                count1++;
-              }
-              else if (a[i][j] == 'B') {
-                numsB[count2] = j;
-                count2++;
-              }
-            }
-            if (numsN[0])
+        for (int sx = index[1]; sx >= 0; sx--) {
+          if (a[index[0]][sx] == 'N') {
+            a[index[0]][sx] = c;
+            a[index[0]][index[1]] = '-';
+            move = true;
+            break moving;
           }
         }
+        for (int dx = index[1]; dx < 8; dx++) {
+          if (a[index[0]][dx] == 'N') {
+            a[index[0]][dx] = c;
+            a[index[0]][index[1]] = '-';
+            move = true;
+            break moving;
+          }
+        }
+        for (int up = index[0]; up >= 0; up--) {
+          if (a[up][index[1]] == 'N') {
+            a[up][index[1]] = c;
+            a[index[0]][index[1]] = '-';
+            move = true;
+            break moving;
+          }
+        }
+        for (int down = index[0]; down < 8; down++) {
+          if (a[down][index[1]] == 'N') {
+            a[down][index[1]] = c;
+            a[index[0]][index[1]] = '-';
+            move = true;
+            break moving;
+          }
+        }
+        break;
+      }
+      case 'N': {
+        for (int sx = index[1]; sx >= 0; sx--) {
+          if (a[index[0]][sx] == 'B') {
+            a[index[0]][sx] = c;
+            a[index[0]][index[1]] = '-';
+            move = true;
+            break moving;
+          }
+        }
+        for (int dx = index[1]; dx < 8; dx++) {
+          if (a[index[0]][dx] == 'B') {
+            a[index[0]][dx] = c;
+            a[index[0]][index[1]] = '-';
+            move = true;
+            break moving;
+          }
+        }
+        for (int up = index[0]; up >= 0; up--) {
+          if (a[up][index[1]] == 'B') {
+            a[up][index[1]] = c;
+            a[index[0]][index[1]] = '-';
+            move = true;
+            break moving;
+          }
+        }
+        for (int down = index[0]; down < 8; down++) {
+          if (a[down][index[1]] == 'B') {
+            a[down][index[1]] = c;
+            a[index[0]][index[1]] = '-';
+            move = true;
+            break moving;
+          }
+        }
+        break;
       }
     }
+    return move;
+  }
+
+  public static boolean endMove (char[][] a) {
+    char c = 'B';
+    int countB = 0, countN = 0;
+
+    if (!move(a, indexB1, c)) {
+      if (!move(a, indexB2, c)) {
+      }
+      else countB++;
+    }
+    else countB++;
+
+    c = 'N';
+
+    if (!move(a, indexN1, c)) {
+      if (!move(a, indexN2, c)) {
+      }
+      else countN++;
+    }
+    else countN++;
+
+    if (countN == 2 || countB == 2) {
+      return true;
+    }
+    else return false;
   }
 
   public static void main(String[] args) {
     char[][] chess = new char[8][8];
+    int maxMoves = 0;
+
     initialise(chess);
     placeB(chess);
     placeN(chess);
-
     print(chess);
 
+    do {
+      maxMoves++;
+    } while (!endMove(chess) && maxMoves < 100);
+
+    print(chess);
   }
 }
