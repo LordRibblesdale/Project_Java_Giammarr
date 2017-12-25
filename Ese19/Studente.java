1b063f0b7ddf610d2d8aa3a5c38ce83a707ac9ac
Domenico Verde <domenico.verde@aol.com>
2017-12-25 15:11:37 +0100

    Change Ese19
    - Edit Studente class

--- Ese19/Studente.java
+++ Ese19/Studente.java
@@ -1,7 +1,36 @@
 public class Studente extends Persona {
   private String matricola;
-  private Esame[] examList = new Esame[];
+  private Esame[] examList;
   private static int numStud = 0;
 
-  public Studente(String nameIn, int year, )
+  public Studente(String nameIn, int year, int no_Exams, int ) {
+    super(nameIn, year);
+    examList = new Esame[no_Exams];
+
+    for (int i = 0; i < no_Exams; i++) {
+      examList[i] = new Esame(nameIn, year);
+    }
+
+    numStud++;
+    matricola = Integer.toString(22222+numStud);
+  }
+
+  public String getMatricola() {
+    return matricola;
+  }
+
+  public void setMatricola(int matricolaIn) {
+    matricola = Integer.toString(matricolaIn);
+  }
+  //examList[n] come Esame class
+  public String toString() {
+    return super.toString() + "Matricola: " + getMatricola() + "\nVoti esami: " + examList[i].toString();
+  }
+
+  public static void main(String[] args) {
+    int i = 3;
+    Studente[] student = new Studente[i];
+
+    for ()
+  }
 }
