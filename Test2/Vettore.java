95d5fe800840121108577f10cf7e750ad2413ab0
Domenico Verde <domenico.verde@aol.com>
2018-02-17 11:53:01 +0100

    - Fix error

--- Test2/Vettore.java
+++ Test2/Vettore.java
@@ -10,7 +10,7 @@ public class Vettore {
 	  	  	  //System.out.println("\ntmp1 " + tmp1);
 	  	  	  for (int h = i; h < a.length; h++) {
 	  	  	  	  tmp2 += a[h];
-	  	  	  }	  
+	  	  	  }
 	  	  	  //System.out.println("tmp2 " + tmp2);
 	  	  	  if (tmp1 > tmp2) {
 	  	  	  	  return i-1;
@@ -18,7 +18,7 @@ public class Vettore {
     	  	  }
     	  	  return -1;
     	  }
-	
+
 	public static int maxVett(int[] a) {
 		int tmp1 = 0, tmpIndex = -1;
 		for (int i = 0; i < a.length; i++) {
@@ -29,16 +29,62 @@ public class Vettore {
 		}
 		return tmpIndex;
 	}
-	
+
 	public static void main(String[] args) {
 		int[] arr = new int[20];
-		
+
 		System.out.print("\nVettore: ");
 		for (int i = 0; i < arr.length; i++) {
 			arr[i] = (int)Math.floor(Math.random()*20)+1;
 			System.out.print(arr[i] + " ");
 		}
-		
+
 		System.out.println("\n" + "L'indice del valore piu' alto nel vettore e': " + maxVett(arr) + "\n" + "L'indice per il quale vale la disequazione (vd.Ese1 per bar(v)) e': " + barVett(arr));
 	}
 }
+=======
+public class Vettore {
+	  public static int barVett(int[] a) {
+	  	  int tmp1, tmp2;
+	  	  for (int i = 1; i < a.length; i++) {
+	  	  	  tmp1 = 0;
+	  	  	  tmp2 = 0;
+	  	  	  for (int j = i-1; j >= 0; j--) {
+	  	  	  	  tmp1 += a[j];
+	  	  	  }
+	  	  	  //System.out.println("\ntmp1 " + tmp1);
+	  	  	  for (int h = i; h < a.length; h++) {
+	  	  	  	  tmp2 += a[h];
+	  	  	  }
+	  	  	  //System.out.println("tmp2 " + tmp2);
+	  	  	  if (tmp1 > tmp2) {
+	  	  	  	  return i-1;
+		   	  }
+    	  	  }
+    	  	  return -1;
+    	  }
+
+	public static int maxVett(int[] a) {
+		int tmp1 = 0, tmpIndex = -1;
+		for (int i = 0; i < a.length; i++) {
+			if (a[i] > tmp1) {
+				tmp1 = a[i];
+				tmpIndex = i;
+			}
+		}
+		return tmpIndex;
+	}
+
+	public static void main(String[] args) {
+		int[] arr = new int[20];
+
+		System.out.print("\nVettore: ");
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = (int)Math.floor(Math.random()*20)+1;
+			System.out.print(arr[i] + " ");
+		}
+
+		System.out.println("\n" + "L'indice del valore piu' alto nel vettore e': " + maxVett(arr) + "\n" + "L'indice per il quale vale la disequazione (vd.Ese1 per bar(v)) e': " + barVett(arr));
+	}
+}
+>>>>>>> before discard
