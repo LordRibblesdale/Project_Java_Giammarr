cc925e74f0a0f86b5119ddc6f321cb9e73c47cce
Domenico Verde <domenico.verde@aol.com>
2018-03-02 16:15:04 +0100

    - Add new method in BubbleSort

--- Algorithms/BubbleSort.java
+++ Algorithms/BubbleSort.java
@@ -8,7 +8,7 @@
  */
 
 public class BubbleSort {
-   public static void bubSort(int[] a) {
+   public static void bubSort(int[] a) {  //checks ALL the array
       int tmp = 0;
 
       for (int i = 0; i < a.length-1; i++) {
@@ -19,6 +19,24 @@ public class BubbleSort {
                a[j] = tmp;
             }
          }
+      }
+   }
+
+   public static void secondBS(int[] a) {
+      boolean isSorted = true;
+      int tmp = 0, i = 0;
+
+      while (i < a.length && (isSorted)) {   //skip if all array is sorted
+         isSorted = false;
+         for (int j = 0; j < a.length-1; j++) {
+            if (a[j] > a[j+1]) {
+               tmp = a[j+1];
+               a[j+1] = a[j];
+               a[j] = tmp;
+            }
+            isSorted = true;
+         }
+         i++;
       }
    }
 }
